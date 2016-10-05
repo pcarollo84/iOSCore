e454eac4085c4bde7392a7aa7a256ea86b5c08d6
Paolo <paolo@pocketsizemedia.co.uk>
2016-10-05 10:05:35 +0100

    added LOG_ASYNC_ENABLED before import fw

--- iOSCore/iOSCore.h
+++ iOSCore/iOSCore.h
@@ -17,6 +17,7 @@ FOUNDATION_EXPORT const unsigned char iOSCoreVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <iOSCore/PublicHeader.h>
 
 #define LOG_LEVEL_DEF ddLogLevel
+#define LOG_ASYNC_ENABLED YES
 
 #import <CocoaLumberjack/CocoaLumberjack.h>
 
